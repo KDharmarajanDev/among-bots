09fa85aecbe97bd50db549490bd6139c34c67418
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-10-10 00:34:09 -0700

    Changed output messages from commands.

--- bot.py
+++ bot.py
@@ -26,21 +26,21 @@ async def crewloss(ctx, *args):
     data = AmongUsData.from_dict(connection_manager.get_data('AmongUs', PlayerData(ctx.message.author.id)),ctx.message.author.id)
     data.modify(AmongUsData.CREW_LOSS,AmongUsData.increase,1)
     connection_manager.update_stats_user('AmongUs',PlayerData(ctx.message.author.id), data)
-    await ctx.send(f'Your crewmate win amount is now {data.crew_mate_losses}.')
+    await ctx.send(f'Your crewmate loss amount is now {data.crew_mate_losses}.')
 
 @client.command(pass_context=True)
 async def impostorwin(ctx, *args):
     data = AmongUsData.from_dict(connection_manager.get_data('AmongUs', PlayerData(ctx.message.author.id)),ctx.message.author.id)
     data.modify(AmongUsData.IMPOSTOR_WIN,AmongUsData.increase,1)
     connection_manager.update_stats_user('AmongUs',PlayerData(ctx.message.author.id), data)
-    await ctx.send(f'Your crewmate win amount is now {data.impostor_wins}.')
+    await ctx.send(f'Your impostor win amount is now {data.impostor_wins}.')
 
 @client.command(pass_context=True)
 async def impostorloss(ctx, *args):
     data = AmongUsData.from_dict(connection_manager.get_data('AmongUs', PlayerData(ctx.message.author.id)),ctx.message.author.id)
     data.modify(AmongUsData.IMPOSTOR_LOSS,AmongUsData.increase,1)
     connection_manager.update_stats_user('AmongUs',PlayerData(ctx.message.author.id), data)
-    await ctx.send(f'Your crewmate win amount is now {data.crew_mate_losses}.')
+    await ctx.send(f'Your impostor loss amount is now {data.crew_mate_losses}.')
 
 #If there is an error, it will answer with an error
 @client.event
