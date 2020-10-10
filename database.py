afaa20188ec71bf65b286b6d5dca0ba6da999e06
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-10-09 22:14:33 -0700

    Fixed MongoDB Code

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 class ConnectionManager:
 
     def __init__(self):
-        self.database = MongoClient(os.environ.get('DATABASE_CONNECTION_URI'))['AmongUs']
+        self.database = MongoClient(os.environ.get('DATABASE_CONNECTION_URI')).GameStats.AmongUs
     
     def get_stats(self, user_id):
         return self.database.find_one({'user_id': user_id})
