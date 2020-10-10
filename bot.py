cff987ca20b5858167e275fed005e05a79edaacb
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-10-09 21:34:10 -0700

    Added basic database functionality

--- bot.py
+++ bot.py
@@ -5,16 +5,23 @@ from discord.ext import commands
 
 TOKEN = os.environ.get('BOT_TOKEN')
 client = commands.Bot(command_prefix = '.')
+connection_manager = database.ConnectionManager()
 
 #answers with the ms latency
-@client.command()
+@bot.command(pass_context=True)
 async def ping(ctx):
     await ctx.send(f'Pong! {round (client.latency * 1000)}ms ')
+
+@bot.command(pass_context=True)
+async def send(ctx, args):
+    connection_manager.set_stats({'user_id': ctx.message.author.id, 'args': args})
 
 #If there is an error, it will answer with an error
 @client.event
 async def on_command_error(ctx, error):
     await ctx.send(f'Error. Try .help ({error})')
     print('test')
+
+
 
 client.run(TOKEN)
--- database.py
+++ database.py
@@ -1,1 +1,13 @@
-import pymongo
+import os
+from pymongo import MongoClient
+
+class ConnectionManager:
+
+    def __init__(self):
+        self.database = MongoClient(os.environ.get('DATABASE_CONNECTION_URI'))['AmongBots']
+    
+    def get_stats(self, user_id):
+        return self.database.find_one({'user_id': user_id})
+
+    def set_stats(self, send_object):
+        self.database.insert(send_object)
